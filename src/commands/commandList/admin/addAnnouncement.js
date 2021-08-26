54a4889a6722f0aeeab7c71914aa53e8a1c5bf18
M4litoChan <krinksers@gmail.com>
2021-08-26 15:13:02 +0000

    Added error exception to sql handler

--- src/commands/commandList/admin/addAnnouncement.js
+++ src/commands/commandList/admin/addAnnouncement.js
@@ -10,15 +10,16 @@ module.exports = new CommandInterface({
 	dm:true,
 
 	execute: async function(p){
-		try{
+		try {
 			let url = p.args[0];
 			let data = await p.DataResolver.urlToBuffer(url);
 			await p.send("This is a test message! Does it look ok?",null,{file:data,name:"announcement.png"});
 			let sql = "INSERT INTO announcement (url) VALUES (?)";
 			await p.query(sql,[url]);
 			await p.send("Added new announcement!");
-		}catch(err){
+		} catch(err) {
 			p.errorMsg(", failed to add announcement");
+			Error.throw("Failed to generate an announcement, exit")
 		}
 	}
 
